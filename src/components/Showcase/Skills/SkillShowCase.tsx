46158954b167aff04d5a4b8963122b4f46c637a7
activationeuron <amitrai_13@yahoo.com>
2020-09-29 00:32:37 +0530

    skills section static done

--- src/components/Showcase/Skills/SkillShowCase.tsx
+++ src/components/Showcase/Skills/SkillShowCase.tsx
@@ -1,12 +1,32 @@
+import { useRef } from "react";
+import { TweenMax } from "gsap";
 interface SkillDetail {
   icon: String;
   text: String;
 }
 
 function SkillShowCase<SkillDetail>(props) {
+  const { text, icons } = props;
+
   return (
     <div className='text-center'>
-      <div>{props.icons[0]}</div>
+      <div className='flex items-center content-center '>
+        <div className='text-xl text-offwhite space-x-1 text-center '>
+          {"<"}
+          {text}
+        </div>
+        {icons.map((icon, index) => {
+          return (
+            <span
+              key={index}
+              id={icon.id}
+              className='mx-1'
+              dangerouslySetInnerHTML={{ __html: icon.icon }}
+            />
+          );
+        })}
+        <span className='text-offwhite'>{"/>"}</span>
+      </div>
     </div>
   );
 }
