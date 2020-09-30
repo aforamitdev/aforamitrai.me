b531b9510d31d77e6b912c348a70406757c872b9
activationeuron <amitrai_13@yahoo.com>
2020-09-30 22:40:42 +0530

    responsive initial

--- src/components/Showcase/Main.tsx
+++ src/components/Showcase/Main.tsx
@@ -2,7 +2,7 @@ import Contacts from "./Contacts";
 import Skills from "./Skills";
 const Main = () => {
   return (
-    <div className='flex flex- w-full h-full '>
+    <div className='md:flex md:flex-1 md:w-full md:h-full sm:inline-block  '>
       <div className='absolute text-offwhite bottom-0 left-0'>
         Under active Devlopment{" "}
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import Navbar from "../components/Navbar";
 import Main from "../components/Showcase/Main";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className='font:sans w-full bg-gray-900 h-screen px-12 relative'>
-      <div className='absolute right-0 top-0 w-full px-12'>
-        <Navbar />
+    <>
+      <Head>
+        <title>Amitkumar Rai</title>
+      </Head>
+
+      <div className='font:sans w-full bg-gray-900 md:h-screen px-12 relative  '>
+        <div className=' hidden sm:hidden md:block  absolute right-0 top-0 w-full px-12 '>
+          <Navbar />
+        </div>
+        <div className=' md:h-screen px-12 sm:space-y-64'>
+          <Main />
+        </div>
       </div>
-      <div className='h-screen px-12'>
-        <Main />
-      </div>
-    </div>
+    </>
   );
 }
