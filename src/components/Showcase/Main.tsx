440d0589694df6bcace7e52cddff9914a63ee600
activationeuron <amitrai_13@yahoo.com>
2020-10-20 01:57:48 +0530

    resonsive for mobile

--- src/components/Showcase/Main.tsx
+++ src/components/Showcase/Main.tsx
@@ -2,12 +2,15 @@ import Contacts from "./Contacts";
 import Skills from "./Skills";
 const Main = () => {
   return (
-    <div className='md:flex md:flex-1 md:w-full md:h-full sm:inline-block  '>
+    <div className='min-h-screen min-w-full mx-auto flex flex-col  lg:flex lg:justify-center '>
+      <div className="mx-auto pb-10 pt-10 space-y-12 lg:flex  lg:space-x-32 ">
+
+      <Contacts />
+      <Skills />
+      </div>
       <div className='absolute text-offwhite bottom-0 left-0'>
         Under active Devlopment{" "}
       </div>
-      <Contacts />
-      <Skills />
     </div>
   );
 };
